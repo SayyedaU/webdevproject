e38ee4b22e70d98f9a81543c34036b1913dd843a
Sayyeda Uzma <sayyedauzma9@gmail.com>
2022-07-25 11:58:13 -0500

    commit 7/25/22

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -97,3 +97,194 @@ d /= 2;
 console.log("this is d:" + d)
 
 //boolean notes on paper
+
+/*if statement
+var n = 23
+if (n == 22) {
+    console.log("this number is 22");
+}
+console.log("this number is not 22");
+*/
+
+/* if else statement
+var n = 23
+if(n==22) {
+    console.log("this number is 22");
+} else {
+    console.log("this number is not 22");
+}
+*/
+
+/*if else is statement 
+var n = 23
+if (n==22){
+    console.log("this number is 22");
+} else if (n==23) {
+    console.log("this number is 23");
+} else {
+    console.log("this number is neither 22 or 23");
+}
+*/
+
+// &&(both) and ||(or) operator 
+var n = 23
+if (n == 22 ||  n == 23) {
+    console.log("n is 22 or 23");
+} else {
+    console.log("n is neither 22 or 23");
+}
+
+var number = "27";
+
+if (number == 47) {
+    console.log("checked: this number is 47");
+} else {
+    console.log("checked: this number isn't 47");
+}
+
+// ++ -- 
+number++;
+number--;
+console.log(number);
+
+// concatenation of strings
+console.log("number = " + number);
+console.log("number is " + number + ". This number is prime.");
+
+// >= <=
+var numCheck = 20;
+if (number <= 10) {
+    console.log("num is less than or equal to 10");
+} else if ( number >= 11) {
+    console.log("num is greater than or equal to 11");
+} else {
+    console.log("this is not a number");
+}
+
+const PET = "dog";
+
+if (PET === "lizard") {
+    console.log("I own a lizard");
+} else if (PET === "dog") {
+    console.log("I own a dog");
+} else if (PET === "cat") {
+    console.log("I own a cat");
+} else {
+    console.log("I don't own a pet");
+}
+
+//switch statements
+switch(PET) {
+    case "lizard":
+        console.log("I own a cat");
+        break;
+    case "dog":
+        console.log("I own a dog");
+        break;
+    case "cat":
+        console.log("I own a cat");
+        break;
+    default:
+        console.log("I don't own a pet");  
+        break;          
+}
+
+// Grouping cases
+const COUNTRY = "Ireland";
+switch(COUNTRY) {
+    case "France":
+    case "Spain":
+    case "Poland":
+    case "Ireland":
+        console.log("This country is in Europe.");
+        break;   
+    case "United States":
+        console.log("This is country is in North America.");
+        break;
+    default:
+        console.log("This country is not in North America or Europe.");
+        break;             
+
+
+}
+
+// global scope
+const ERRAND = "Going Shopping";
+//block scope (the message can only be used in the scope)
+switch (ERRAND) {
+
+    case "Going to the Dentist": {
+        let message = " I hate going to the dentist";
+        console.log(message);
+        break;
+    }
+    case "Going Shopping": {
+        let message = "I love to shop!";
+        console.log(message);
+        break;
+    }    
+    case "Chores": {
+        let message = "I hate doing chores";
+        console.log(message);
+        break;
+    }
+    default: {
+        let message = "No errands";
+        console.log(message);
+        break;
+    }        
+}
+
+// structure of a function
+// alert, confirm, prompt
+function myFunction() {
+    //whatever
+    //alert("I am an alert box!");
+
+
+    /*var txt;
+    if (confirm("Press a button")) {
+        txt = "You pressed OK!";
+    } else {
+        txt = "You pressed Cancel!";
+    }
+document.getElementById("demo").innerHTML = txt;
+}
+*/
+
+let text;
+let person = prompt("Please enter your name:","Baymax");
+if (person == null || person == "") {
+    text = "User cancelled the prompt";
+} else {
+    text = "Hello " + person + "! How are you today?";
+}
+document.getElementById("demo"). innerHTML = text;
+}
+
+/*while loop
+var i = 0;
+console.log("Starting Loop");
+while (i < 10) {
+    console.log("The number is " + i);
+    i++;
+}
+console.log("End of Loop");
+*/
+
+/*Do While Loop
+var i = 10;
+console.log("Starting loop");
+do {
+    console.log("The number is "+ i);
+    i--;
+} while ( i > 0);
+console.log("Ending Loop");
+*/
+
+var count;
+console.log("Start Loop");
+for(count = 0; count < 10; count++){
+    console.log("Current Count:" + count);
+}
+console.log("Loop End");
